e0339f3b1fc01c05da51e5efd3f724fd70035378
Bartol <bartolx555@gmail.com>
2026-07-16 20:49:47 +0200

    mild formatting

--- src/main/java/xyz/segurapass/sdk/service/UptimeService.java
+++ src/main/java/xyz/segurapass/sdk/service/UptimeService.java
@@ -3,5 +3,7 @@ package xyz.segurapass.sdk.service;
 import xyz.segurapass.sdk.exception.SegurapassSdkException;
 
 public interface UptimeService {
+
     boolean getUptime(String serverUrl) throws SegurapassSdkException;
+
 }
